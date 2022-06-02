5248b742a2e71b8574f5f3a3affbb875b21b09f4
irishsea <iriserggeeva@gmail.com>
2022-06-02 09:19:30 +0300

    определение дубликатов с помощью коллекции hashMap

---
+++ src/main/java/com/irishsea/Main.java
@@ -0,0 +1,32 @@
+package com.irishsea;
+
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> parserMap = new HashMap<>();
+        File fileXml = new File("address.xml");
+//        try {
+//            FileInputStream fis = new FileInputStream(fileXml);
+//            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+//            BufferedReader reader = new BufferedReader(isr);
+//            String str;
+//            while ((str = reader.readLine()) != null){
+//                System.out.println(str);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        XMLParser xmlParser = new XMLParser(fileXml);
+        xmlParser.parse(parserMap);
+        for (String key : parserMap.keySet()) {
+            int count = parserMap.get(key);
+            if (count > 1) {
+                System.out.println(key + " " + count);
+            }
+        }
+    }
+}
